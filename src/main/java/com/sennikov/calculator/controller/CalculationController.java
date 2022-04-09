74047cb48a5d894f636397f270219d0e6b701b45
asennikov <EH85P9NCeWMBk5Qy>
2022-04-09 22:03:20 +0300

    Exception handler

--- src/main/java/com/sennikov/calculator/controller/CalculationController.java
+++ src/main/java/com/sennikov/calculator/controller/CalculationController.java
@@ -2,49 +2,59 @@ package com.sennikov.calculator.controller;
 
 import com.sennikov.calculator.dto.Response;
 import com.sennikov.calculator.dto.iResponse;
-import com.sennikov.calculator.service.CalculateService;
+import com.sennikov.calculator.service.CalculationService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Api("Calculate controller")
-@RestController("api/v1/calculate")
-public class CalculateController {
+@RestController("api/v1/сalculation")
+public class CalculationController {
 
-    public CalculateController(CalculateService calculateService) {
-        this.calculateService = calculateService;
+    public CalculationController(CalculationService calculationService) {
+        this.calculationService = calculationService;
     }
 
-    private final CalculateService calculateService;
+    private final CalculationService calculationService;
 
     @ApiOperation(value = "Send addition command")
     @RequestMapping(path = "/addition", method = RequestMethod.GET)
     public iResponse addition(@RequestParam("firstNumber") int firstNumber, @RequestParam("secondNumber") int secondNumber) {
-        return new Response<>(calculateService.add(firstNumber, secondNumber));
+        return new Response<>(calculationService.add(firstNumber, secondNumber));
     }
 
     @ApiOperation(value = "Send subtraction command")
     @RequestMapping(path = "/subtraction", method = RequestMethod.GET)
     public iResponse minus(@RequestParam("firstNumber") int firstNumber, @RequestParam("secondNumber") int secondNumber) {
-        return new Response<>(calculateService.minus(firstNumber, secondNumber));
+        return new Response<>(calculationService.minus(firstNumber, secondNumber));
     }
 
     @ApiOperation(value = "Send multiply command")
     @RequestMapping(path = "/multiply", method = RequestMethod.GET)
     public iResponse multiply(@RequestParam("firstNumber") int firstNumber, @RequestParam("secondNumber") int secondNumber) {
-        return new Response<>(calculateService.multiply(firstNumber, secondNumber));
+        return new Response<>(calculationService.multiply(firstNumber, secondNumber));
     }
 
     @ApiOperation(value = "Send divide command")
     @RequestMapping(path = "/divide", method = RequestMethod.GET)
     public iResponse divide(@RequestParam("firstNumber") int firstNumber, @RequestParam("secondNumber") int secondNumber) {
-        try {
-            return new Response<>(calculateService.divide(firstNumber, secondNumber));
-        } catch (ArithmeticException ex) {
-            return new Response<>("Error. Division by zero");
+        if (secondNumber == 0) {
+            throw new IllegalArgumentException("Division by zero");
         }
+            return new Response<>(calculationService.divide(firstNumber, secondNumber));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public Response<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return new Response<>("Error. Division by zero");
+    }
+
+    @ExceptionHandler(ArithmeticException.class)
+    public Response<String> handleArithmeticException(ArithmeticException ex) {
+        return new Response<>("Error. Too big numbers");
     }
 }
--- src/main/java/com/sennikov/calculator/service/CalculationService.java
+++ src/main/java/com/sennikov/calculator/service/CalculationService.java
@@ -3,22 +3,21 @@ package com.sennikov.calculator.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CalculateService {
+public class CalculationService {
 
     public int add(int firstNumber, int secondNumber) {
-        return firstNumber + secondNumber;
+        return Math.addExact(firstNumber, secondNumber);
     }
 
     public int minus(int firstNumber, int secondNumber) {
-        return firstNumber - secondNumber;
+        return Math.subtractExact(firstNumber, secondNumber);
     }
 
     public int multiply(int firstNumber, int secondNumber) {
-        return firstNumber * secondNumber;
+        return Math.multiplyExact(firstNumber, secondNumber);
     }
 
     public double divide(int firstNumber, int secondNumber) {
         return (double) firstNumber / (double) secondNumber;
-//        } catch (ArithmeticException ex) {
     }
 }
